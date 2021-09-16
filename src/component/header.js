a932c7f34e51b0fd3d59d60fda7b7e09d0dd2785
pkmaneri <praveen.kumar.maneri@gmail.com>
2021-09-16 10:45:38 +0530

    fc

--- src/component/header.js
+++ src/component/header.js
@@ -14,11 +14,12 @@ function Header() {
 
     return (
         <Stack>
-            
+
             <Circle position="absolute" bg="blue.100" opacity="0.1"
                 w="300px" h="300px" alignSelf="flex-end" />
             <Flex direction={isNotSmallerScreen ? "row" : "column"}
                 spacing="200px" p={isNotSmallerScreen ? "32" : "0"}
+                
                 alignSelf="flex-start">
                 <Box mt={isNotSmallerScreen ? "0" : 16} align='flex-start'>
                     <Text fontSize="5xl" fontWeight="semibold">Hi, I am</Text>
--- src/component/profile.js
+++ src/component/profile.js
@@ -6,10 +6,8 @@ import { DiCodeigniter, DiAndroid, DiWebplatform } from 'react-icons/di'
 
 
 function Profile() {
-
+    
     const [isNotSmallerScreen] = useMediaQuery("(min-width:600px)");
-
-
     return (
         <Flex direction={isNotSmallerScreen ? "row" : "column"} w="100%"
             maxWidth={{ base: "100vh", md: "130vh", lg: "130vh", xl: "130vh" }}>
